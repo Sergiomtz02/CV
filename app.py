f75e0231cab43c9cdb6145c4ff977e9715d6b1ac
Sergiomtz02 <0246737@up.edu.mx>
2024-08-20 18:39:08 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+
+import streamlit as st
+
+st.markdown ("# Sergio Martínez Escobedo")
+st.markdown ("## Administración y Finanzas")
+st.image ("/Users/Sergio/Desktop/Archivos de Python/Imagen 18-08-24 a las 23.09.jpg", width=200)
+
+st.markdown ("----------------")
+
+st.markdown ("# Acerca de mi")
+st.markdown ("Actualmente curso el séptimo semestre de la carrera de Administración y Finanzas en la Universidad Panamericana, Campus Guadalajara con 21 años de edad. Poseo un año de experiencia en áreas administrativas y contables, demostrando ser una persona paciente, responsable y comprometida con mis objetivos. Disfruto trabajar en equipo y contribuir a la resolución de conflictos")
+
+st.markdown ("---------------")
+
+st.markdown ("# Idiomas")
+st.markdown ("- Español")
+st.markdown ("- Inglés (Intermedio)")
+
+st.markdown ("---------------")
+
+
+st.markdown ("# Contacto")
+st.markdown ("- +52 492 218 9969")
+st.markdown ("- Av.Empresarios 236, Zapopan, Jalisco, 45116")
+st.markdown ("- serch.mtz88@hotmail.com")
+
+st.markdown ("---------------")
+
+
+st.markdown("# Experiencia Laboral")
+st.markdown("## Olexo Foods (2023-2024)")
+
+st.markdown ("### Responsabilidades:")
+st.markdown("- Uso de Contpaq")
+st.markdown("- Registro de pedidos")
+st.markdown("- Registro de cotizaciones")
+st.markdown("- Registro de facturas")
+st.markdown("- Registro de gastos e ingresos")
+st.markdown("- Elaboración de puntos de equilibrio de productos")
+
+st.markdown("## ENNOVI 2024")
+
+st.markdown ("### Responsabilidades:")
+st.markdown("- Uso de SAP")
+st.markdown("- Registro de facturas de egreso")
+st.markdown("- Registro de fletes")
+st.markdown("- Apoyo en cuentas por Pagar")
+st.markdown("- Apoyo en conteo de inventario")
+
+st.markdown ("---------------")
+
+st.markdown ("## Habilidades")
+st.markdown ("- Manejo de Excel")
+st.markdown ("- Trabajo en equipo")
+st.markdown ("- Aprendizaje rápido")
+st.markdown ("- Orientación a buenos resultados")
+st.markdown("- Capacidad de trabajar con honorarios flexibles")
+
+st.markdown ("---------------")
+
+st.markdown ("## Educación")
+st.markdown ("### Secundaria y preparatoria")
+st.markdown ("- Colegio Margil Zacatecas (2015-2021)")
+st.markdown ("- Bachiller económico-administrativo")
+st.markdown ("### Licenciatura 2021-Actual")
+st.markdown ("- Universidad Panamericana Campus Guadalajara")
+st.markdown ("- Administración y Finanzas")
